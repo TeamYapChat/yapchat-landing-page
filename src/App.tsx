86df7ef4ea75002bc0cdb33b2bb849bfd3e097fb
zachattack168 <zohlenforst@gmail.com>
2025-03-03 13:12:16 -0600

    feat: Add background image and set up nav bar location

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,26 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import bgImg from './assets/landingpagebg.jpg'
 import './App.css'
 
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main className="grid h-screen w-full overflow-hidden" style={{ backgroundImage: `url(${bgImg})` }}>
+      <img src={bgImg} className='absolute inset-0 object-cover w-full h-full -z-20'/>
+      {/* Overlay with blur */}
+      <div className="absolute inset-0 bg-black/25 backdrop-blur-sm flex items-center justify-center">
+        <div className="relative h-full w-full border-gray-400 border-4">
+          {/* NavBar */}
+          <div className='relative h-1/10 w-full top-0 border-white border-4'>
+            <p> weeeeee </p>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </main>
+  );
 }
 
 export default App
