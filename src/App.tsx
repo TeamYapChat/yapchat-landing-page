afb033658ef2ee4eb448fc09317ddf59589b9dbd
zachattack168 <zohlenforst@gmail.com>
2025-03-05 10:07:22 -0600

    chore: Remove unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+//import { useState } from 'react'
 import bgImg from './assets/landingpagebg.jpg'
 import './App.css'
 
