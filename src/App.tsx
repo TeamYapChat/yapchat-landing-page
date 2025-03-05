d33b53d06a9115584bd76c760c467825a70022b9
KhiemNguyen15 <khiemsa15@gmail.com>
2025-03-05 12:33:21 -0600

    refactor: Reformat code with Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-//import { useState } from 'react'
-import bgImg from './assets/bg_login.png'
-import TransparentButton from './components/TransparentButton'
-import './App.css'
-
+import bgImg from "./assets/bg_login.png";
+import TransparentButton from "./components/TransparentButton";
+import "./App.css";
 
 function App() {
-
   return (
-    <main className="grid h-screen w-full overflow-hidden" style={{ backgroundImage: `url(${bgImg})` }}>
-      <img src={bgImg} className='absolute inset-0 object-cover w-full h-full -z-20'/>
+    <main
+      className="grid h-screen w-full overflow-hidden"
+      style={{ backgroundImage: `url(${bgImg})` }}
+    >
+      <img
+        src={bgImg}
+        className="absolute inset-0 object-cover w-full h-full -z-20"
+      />
       {/* Overlay with blur */}
       <div className="absolute inset-0 bg-black/25 backdrop-blur-sm flex items-center justify-center">
         <div className="relative h-full w-full ">
@@ -18,8 +21,8 @@ function App() {
               <p>YapChat Logo Goes Here</p>
             </div>
             <div className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2">
-              <TransparentButton name="Support"/>
-              <TransparentButton name="Discover"/>
+              <TransparentButton name="Support" />
+              <TransparentButton name="Discover" />
             </div>
             <div className="absolute top-1/2 right-10 -translate-y-1/2">
               <TransparentButton name="Login" />
@@ -31,4 +34,4 @@ function App() {
   );
 }
 
-export default App
+export default App;
